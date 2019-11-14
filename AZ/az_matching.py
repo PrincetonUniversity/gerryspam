e0a9a050f25ba20aa676673bd304d09a0b3a5826
Hope Johnson <hopecaneel@gmail.com>
2019-11-14 11:47:35 -0500

    verify VEST 2018 data against wiki dat

--- AZ/az_matching.py
+++ AZ/az_matching.py
@@ -12,12 +12,15 @@ merged_2016.head()
 merged_2016.groupby(['CDE_COUNTY']).sum()
 # matches up with wikipedia results, yay!
 
-# 
 # 2018 matching 
-#
+# 
 # election results from open elections
 url = 'https://raw.githubusercontent.com/openelections/openelections-data-az/master/2018/20181106__az__general__precinct.csv'
 elec = pd.read_csv(url)
 apache = elec.loc[elec['county'] == 'Apache']
 len(apache.precinct.unique())
 
+merged_2018_path = pathlib.Path.home() / "projects" / "AZ" / "az_2018" / "az_2018.shp"
+merged_2018 = gpd.read_file(merged_2018_path)
+merged_2018.head()
+merged_2018.groupby(['CDE_COUNTY']).sum()
