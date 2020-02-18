5ffa880caef1b55e07cbfea2f5dbed97f982da0e
hwheelen <hwheelen@berkeley.edu>
2020-02-18 11:03:18 -0500

    adding Hannah's new scripts

---
+++ GA/geocoded_voter_roll_to_shp_GA.py
@@ -0,0 +1,46 @@
+import time
+import pandas as pd
+import geopandas as gpd
+import math
+import shapely as shp
+import warnings
+import numpy as np
+warnings.filterwarnings("ignore")
+import helper_tools as ht
+import sys
+sys.path.append('/Users/hwheelen/Documents/GitHub/gerrymander-geoprocessing/areal_interpolation')
+import areal_interpolation as ai
+from collections import Counter
+
+
+
+df_tot = pd.DataFrame()
+# loop through geocoded voter rolls and combine into one dataframe
+
+geo_path = "/Volumes/GoogleDrive/Shared drives/princeton_gerrymandering_project/geocoding/Geocoded States from Civis Voterroll/geocoded_GA.csv"
+df_geo = pd.read_csv(geo_path)
+# Only keep rows that matches were found
+df_geo = df_geo[df_geo['is_match'] == 'Match']
+
+# create cenus block geoid
+df_geo['GEOID10'] = df_geo['state_fips'].astype(int).map(str).str.zfill(2) + \
+                    df_geo['county_fips'].astype(int).map(str).str.zfill(3) + \
+                    df_geo['tract'].astype(int).map(str).str.zfill(6) + \
+                    df_geo['block'].astype(int).map(str).str.zfill(4)
+# create dataframe by dropping unnecessary columns in df_geo dataframe
+df = df_geo[['GEOID10','county_fips','vb_vf_precinct_id', 'vb_vf_precinct_name','vb_vf_national_precinct_code', 
+             'voter_status', 'vf_reg_cd','vf_reg_hd', 'vf_reg_sd']]
+df['precinct'] = df['vb_vf_national_precinct_code']
+#df = df[['GEOID10','precinct']]
+
+df = df.groupby('GEOID10')['precinct'].apply(list).apply(Counter)
+df = pd.DataFrame(df)
+df.to_csv('/Volumes/GoogleDrive/Shared drives/princeton_gerrymandering_project/OpenPrecincts/States for site/Georgia/VR Shapefile/blocks/GAblocksVRprecinct.csv')
+
+
+#for county by county
+county = 299.
+df_county = df.loc[df.county_fips == 299.]
+df_county = df_county.groupby('GEOID10')['vb_vf_precinct_id',].apply(list).apply(Counter)
+df_county = pd.DataFrame(df_county)
+df_county.to_csv('/Volumes/GoogleDrive/Shared drives/princeton_gerrymandering_project/mapping/GA/Precinct Data/Ware County/WareCivisVR.csv')
