3d7d86fac13c59554c137939888559da5194c34c
Hope Johnson <hopecaneel@gmail.com>
2020-03-16 14:57:38 -0400

    finalizing

--- AR/final_merge.py
+++ AR/final_merge.py
@@ -6,15 +6,26 @@ import re
 shp_path = "/Users/hopecj/projects/AR/Shapefiles/4_merged/ar18.shp"
 elec_path = "/Users/hopecj/projects/gerryspam/AR/AR_G18.csv"
 
-
 elec_df = pd.read_csv(elec_path)
 shp_df = gpd.read_file(shp_path)
 
 elec_df["elec_loc_prec"] = elec_df["county"] + "," + elec_df["precinct"].str.lower()
-
 shp_df["shp_loc_prec"] = shp_df["county_nam"] + "," + shp_df["PREC"].str.lower()
-
 out = shp_df.merge(elec_df, left_on='shp_loc_prec', right_on='elec_loc_prec', how='outer')
 
-out = out.drop(["geometry"], axis=1)
-out.to_csv("/Users/hopecj/projects/gerryspam/AR/testing_merged.csv")
+out = out[['county_nam', 'state_fips', 'county_fip', 
+           'PREC', 'precinct_x',  
+           'G18DGOV', 'G18LGOV', 
+           'G18RGOV', 'G18DATG',
+           'G18RATG', 'G18LATG', 
+           'G18DSecS', 'G18LSecS', 
+           'G18RSecS', 'G18RTRES', 'G18LTRES', 
+           'geometry']]
+
+out = out.rename(columns={"precinct_x": "precinct_old", 
+                       "PREC": "precinct"})
+
+out.to_file("/Users/hopecj/projects/AR/Shapefiles/5_final/ar18.shp")
+
+# out = out.drop(["geometry"], axis=1)
+# out.to_csv("/Users/hopecj/projects/gerryspam/AR/testing_merged.csv")
--- AR/run.sh
+++ AR/run.sh
@@ -1,9 +1,4 @@
 cd ~/projects/gerryspam/AR
-# python3
-
-# exec(open('edit_prec_names.py').read())
-# exec(open('precinct_dissolve.py').read())
-# exec(open('ms_change.py').read())
 
 python3 edit_prec_names.py
 python3 precinct_dissolve.py
