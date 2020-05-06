a5397cfd1c6929f012d88bf83a98a4d214dd902b
Hope Johnson <hopecaneel@gmail.com>
2020-05-06 10:42:05 -0400

    add bg labbels to census blocks

--- MO/prec_to_blocks.py
+++ MO/prec_to_blocks.py
@@ -4,17 +4,18 @@ import maup
 import matplotlib
 
 small = gpd.read_file('/Users/hopecj/projects/gerryspam/MO/dat/tabblock2010_29_pophu/tabblock2010_29_pophu.shp')
-large = gpd.read_file('/Users/hopecj/projects/gerryspam/MO/dat/mo_prec_labeled/mo_prec_labeled_nopop.shp')
+large = gpd.read_file('/Users/hopecj/projects/gerryspam/MO/dat/tl_2013_29_bg/tl_2013_29_bg.shp')
+# large = gpd.read_file('/Users/hopecj/projects/gerryspam/MO/dat/mo_prec_labeled/mo_prec_labeled_nopop.shp')
 
 small = small.to_crs(large.crs)
 
 # target_columns should be name of disaggregation column from `large`
 # start time: 12:31 pm
 # end time: 1:30 pm
-agg = ai.aggregate(small, large, target_columns=['loc_prec'])[0] # this takes a while!
+agg = ai.aggregate(small, large, target_columns=['GEOID'])[0] # this takes a while!
 
 agg = agg[['STATEFP10', 'COUNTYFP10', 'TRACTCE10', 'BLOCKCE',
        'BLOCKID10', 'PARTFLG', 'HOUSING10',
-       'POP10', 'loc_prec', 'geometry']]
+       'POP10', 'GEOID', 'geometry']]
 
-agg.to_file('/Users/hopecj/projects/gerryspam/MO/dat/blocks_with_prec/mo_blocks_with_prec.shp')
+agg.to_file('/Users/hopecj/projects/gerryspam/MO/dat/blocks_with_bg/blocks_with_bg.shp')
