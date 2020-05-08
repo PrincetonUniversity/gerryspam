54e3ca4259681b73d182df19d47c37e8858b868d
Hope Johnson <hopecaneel@gmail.com>
2020-05-08 14:49:18 -0400

    at long last figured out prec space remove

--- NJ/elec-name-edits.py
+++ NJ/elec-name-edits.py
@@ -21,11 +21,38 @@ def ignore_special(df):
 """
 county-specific edit functions
 """
+# edit precincts with matching issues
+countyToCountyCleaner = {
+    "033": edit_033,
+}
+
 # concat precinct words for sneaky precincts
 # (e.g.: "salem north", "salem east" within the same county)
+def rm_space(row, prec):
+    with_space = prec + ' '
+    regex = re.compile(with_space)
+    return [regex.sub(prec, value) for value in row]
+
 def edit_033(row):
-    regex = re.compile('salem ')
-    return [regex.sub('salem', value) for value in row]
+    return rm_space(row, 'salem')
+
+def edit_027(row):
+    precs = ['salem', 'chester']
+    to_replace = {(prec + ' '):prec for prec in precs}
+    out = row.replace(to_replace, regex=True)
+    out_list = out.tolist()
+    print(type(out_list))
+    return out_list
+
+
+d = {'prec': ["chester east thing", "p", "salem west"], 'col2': ["dog", 4, "cat"]}
+df = pd.DataFrame(data=d)
+
+m_out = edit_033(df['prec'])
+
+x_out = edit_027(df['prec'])
+
+        
 
 
 
@@ -36,7 +63,8 @@ direct data cleaning
 elec_16 = pd.read_csv('/Users/hopecj/projects/gerryspam/NJ/dat/NJ_G16_OpenElex.csv')
 
 # if using open elections data
-elec_16 = pd.concat([elec_16, elec_16['loc_prec'].str.split(',', expand=True)], axis=1)
+# split apart county and precinct (only 1 comma split)
+elec_16 = pd.concat([elec_16, elec_16['loc_prec'].str.split(',', 1, expand=True)], axis=1)
 elec_16 = elec_16.rename(columns={0: "countynam", 1: "precinct"})
 elec_16['precinct'] = elec_16['precinct'].str.lower()
 crosswalk = pd.read_csv('/Users/hopecj/projects/gerryspam/NJ/dat/nj-county-crosswalk.csv')
@@ -49,30 +77,32 @@ print(elec_16.shape)
 elec_16 = ignore_special(elec_16)
 print(elec_16.shape) # got rid of 1238 rows
 
-# edit precincts with matching issues
-countyToCountyCleaner = {
-    "033": edit_033,
-}
-
 clean_elec = elec_16.sort_values(by=['county_fips'])
-
 counties = pd.Series(clean_elec['county_fips']).unique()
 clean_elec["prec_matching"] = clean_elec["precinct"].copy()
 clean_elec.set_index(['county_fips', 'precinct'], inplace=True)
-print("duplicated indices", clean_elec[clean_elec.index.duplicated()])
+# print("duplicated indices", clean_elec[clean_elec.index.duplicated()])
+
+# # FOR TESTING
+# county_dat = clean_elec.loc['033']
+# changed = countyToCountyCleaner.get(county, lambda x: x)(county_dat['prec_matching'])
+# print("changed", changed)
+# county_dat['prec_matching'] = changed
+# print(county_dat['prec_matching'])
 
 for county in counties:
     county_dat = clean_elec.loc[county]
-    changed = countyToCountyCleaner.get(county, lambda x: x)(county_dat)
+    changed = countyToCountyCleaner.get(county, lambda x: x)(county_dat['prec_matching'])
+    county_dat['prec_matching'] = changed
     clean_elec.update(county_dat)
 
 # continue with the general clean 
-elec_16['elec_loc_prec'] = elec_16['county_fips'].astype(str) + "," + elec_16['prec_matching'].astype(str)
-prec_split = elec_16['prec_matching'].str.split(expand=True)
-elec_16['prec_word1'] = prec_split[0]
-elec_16 = elec_16.drop([2, 3, 'Unnamed: 6'], axis=1)
+clean_elec['elec_loc_prec'] = clean_elec['county_fips'].astype(str) + "," + clean_elec['prec_matching'].astype(str)
+prec_split = clean_elec['prec_matching'].str.split(expand=True)
+clean_elec['prec_word1'] = prec_split[0]
+clean_elec = clean_elec.drop([2, 3, 'Unnamed: 6'], axis=1)
 
 # make column of first word from precinct name and numbers 
-elec_nums = ignore_alpha(elec_16['elec_loc_prec'])
-elec_16["elec_loc_prec_nums"] = elec_nums
-elec_16["elec_loc_prec_code"] = elec_16['elec_loc_prec_nums'].astype(str) + '_' + elec_16['prec_word1']
+elec_nums = ignore_alpha(clean_elec['elec_loc_prec'])
+clean_elec["elec_loc_prec_nums"] = elec_nums
+clean_elec["elec_loc_prec_code"] = clean_elec['elec_loc_prec_nums'].astype(str) + '_' + clean_elec['prec_word1']
