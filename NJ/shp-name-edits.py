027ca95049ce2f4e2a266a904884920819aee746
Hope Johnson <hopecaneel@gmail.com>
2020-05-08 19:08:31 -0400

    shape prec name updates NJ

--- NJ/shp-name-edits.py
+++ NJ/shp-name-edits.py
@@ -30,7 +30,7 @@ def rm_space(row, prec_in, prec_out=None):
     return [regex.sub(replace, value) for value in row]
 
 # same as the above but deals with multiple precincts within one county
-def rm_space_multiples(row, mult_precincts):
+def rm_space_multiples(row, *mult_precincts):
     to_replace = {(prec + ' '):prec for prec in mult_precincts}
     out = row.replace(to_replace, regex=True)
     out_list = out.tolist()
@@ -42,11 +42,30 @@ county-specific edit functions
 # edit precincts with matching issues
 countyToCountyCleaner = {
     "033": edit_033,
+    "027": edit_027,
+    "019": edit_019,
 }
 
 def edit_033(row):
     return rm_space(row, 'salem city', 'salem')
 
+def edit_027(row):
+    precs = ['salem', 'chester', 'mendham', 'morris']
+    return rm_space_multiples(row, precs)
+
+# need to come back and change this one
+# so that it's possible to put "in" and "out" options on the multiple 
+def edit_019(row):
+    to_replace = ['clinton ', 'lebanon borough']
+    replace_with=['clinton', 'lebanonboro']
+    out = df['prec'].replace(to_replace, replace_with, regex=True)
+    return out 
+
+d = {'prec': ["clinton hill", "p", "lebanon borough west", 'lebanon boro south', 'salem hill east', 'chester thing 2', 'chester'], 'col2': ["dog", 4, "cat", "rabbit", 3, 5, 2]}
+df = pd.DataFrame(data=d)
+
+print(df['prec'])
+u_out = edit_019(df['prec'])
 
 """
 direct data cleaning
