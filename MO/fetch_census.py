4b6318312f98f5c87a0e995645c13052fdf1fb21
Hope Johnson <hopecaneel@gmail.com>
2020-06-08 18:01:25 -0400

    add example

--- MO/fetch_census.py
+++ MO/fetch_census.py
@@ -1,5 +1,5 @@
 '''
-Expected input: Shapefile of Missori census blocks with total population in each block
+Expected input: Shapefile of Missouri census blocks with total population in each block
 Expected output: Shapefile of Missouri census blocks with black population in each
 '''
 
@@ -18,9 +18,12 @@ features = []
 # https://api.census.gov/data/2010/dec/sf1/variables.html 
 black_pop = c.sf1.geo_block(('NAME', 'P003003'), block_shape['geometry'], 2010)
 
-for block_geojson, block_data in black_pop:
-     block_geojson['properties'].update(block_data)
-     features.append(block)
-     
+# for block_data in black_pop:
+#      block_geojson['properties'].update(block_data)
+#      features.append(block)
+
 my_shape_with_new_data_geojson = {'type': "FeatureCollection", 'features': features}
 print(my_shape_with_new_data_geojson)
+
+# https://github.com/datamade/chicago-community-area-ward-demographics/blob/master/scripts/calculate_demographics.py
+# example for how to use it
