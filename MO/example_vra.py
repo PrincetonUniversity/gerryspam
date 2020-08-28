613b66cab33393dec3a06cafe1671a9321eb92a9
Hope Johnson <hopecaneel@gmail.com>
2020-08-28 15:55:15 -0400

    rm to previous state

--- MO/example_vra.py
+++ MO/example_vra.py
@@ -1,10 +1,14 @@
 from gerrychain import Partition, Graph
+import math
+from gerrychain.metrics.compactness import compute_polsby_popper, polsby_popper
 
 graph = Graph()
 graph.add_edges_from([(0, 1), (1, 2), (2, 0)]) # Arbitrary. copied this from Updaters documentation
 
 POP_COL = 'vap'
 VRA_POP_COL = 'bvap'
+AREA_COL = 'area'
+PERIM_COL = 'perimeter'
 
 '''
 Fake data here. Node 0 and 2 have enough to qualify alone, but pairing/districting
@@ -47,4 +51,3 @@ assert partition.num_vra_districts == 2
 # This assignment (2nd argument) has Nodes 0 and 1 in the same district, 2 alone.
 partition = Partition(graph, {0:0, 1:0, 2:1}, {"num_vra_districts": num_vra_districts})
 assert partition.num_vra_districts == 1
-
